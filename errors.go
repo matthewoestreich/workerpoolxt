20f24bc09d087bce98e9bacfd13da1095d52e9ac
Matt Oestreich <matthewpoestreich@gmail.com>
2025-06-28 18:57:28 -0500

    go fmt

--- errors.go
+++ errors.go
@@ -2,10 +2,10 @@ package workerpoolxt
 
 // JobPanicError is what gets thrown during job panic recovery
 type JobPanicError struct {
-    Job Job
-    Message string
+	Job     Job
+	Message string
 }
 
 func (e JobPanicError) Error() string {
-    return "JobPanicError \"" + e.Job.Name + "\" " + e.Message
+	return "JobPanicError \"" + e.Job.Name + "\" " + e.Message
 }
